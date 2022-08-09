574c80f6eee5bb821c3666e61c98fb7ea9e7c4b0
Brian Denmark <testingarea479@gmail.com>
2022-08-09 12:14:48 -0700

    completed challenges 2 and 3 together

--- dict.py
+++ dict.py
@@ -4,6 +4,7 @@
 # We need to put each team into a lost of Player object instancs 
 # so that we can use methods related to players. 
 
+# players imaginary API info
 players = [
     {
         "position": "small forward", 
@@ -40,16 +41,33 @@ players = [
     }
 ]
 
+
 class Player:
+    # created an empty list to store our intances of Player objects.
+    player_instances = []
+
     def __init__(self, dict):
         self.name = dict["name"]
         self.age = dict["age"]
         self.position = dict["position"]
         self.team = dict["team"]
+        Player.player_instances.append(self)
 
-# KD = Player(players[0])
+    @classmethod
+    def show_instances(cls):
+        for i in range(0,len(cls.player_instances)):
+            print(i.dict["name"])
+        return cls
 
-# print(KD.name)
-# print(KD.age)
-# print(KD.position)
-# print(KD.team)
+kd = Player(players[0])
+jasonT = Player(players[1])
+kyrieI = Player(players[2])
+
+players_iterated = []
+for dict in players:
+    players_iterated.append(Player(dict))
+
+print(players_iterated)
+
+for i in players_iterated:
+    print(i.name)
